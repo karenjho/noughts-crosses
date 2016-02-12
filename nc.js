f2736c699fecfb4314d798e87deb6d34c067c363
karenjho <hokarenj@gmail.com>
2016-02-12 13:56:53 -0500

    Magic square (5x5) values

--- nc.js
+++ nc.js
@@ -1,5 +1,14 @@
 $(document).on('ready', function() {
-  
+
 
 
 })
+
+// Magic Square (5 x 5)
+// Sum of any row, column, or diagonal: 65
+
+// 11  18  25   2   9
+// 10  12  19  21   3
+//  4   6  13  20  22
+// 23   5   7  14  16
+// 17  24   1   8  15
