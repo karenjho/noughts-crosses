5437f549822e918ee82174a5c6a9578e1594cfef
karenjho <hokarenj@gmail.com>
2016-02-12 16:24:23 -0500

    Calculate whether sums of o's or x's equal magic square number

--- nc.js
+++ nc.js
@@ -8,7 +8,7 @@ $(document).on('ready', function() {
 
     // We will need to perform multiples functions on the clicked td
     // So we will store the jQuery function $(this) in a variable
-    var self = $(this)
+    var self = $(this);
 
     // How to alternate between X and O?
     // Assign X or O depending on whether the turn is even or odd
@@ -26,8 +26,55 @@ $(document).on('ready', function() {
       self.html('X').addClass('x');
     }
 
-    turn++ // Increment the turn counter
+    // Figure out whether the turn was a winning one
+    if (turn > 7) {
+      checkWinningTurn();
+    }
+
+    turn++; // Increment the turn counter
   });
+
+  // Get all id values for all objects of a specific class
+  function getIds(cellClass) {
+    return $(cellClass).map(function(index) {
+      return $(this).attr('id');
+    });
+  }
+
+  // Strip the id numbers from the id values array
+  function getIdNums(idArray) {
+    return $(idArray)
+      .map(function(index) {
+        return parseInt(this.substring(5));
+      })
+      .get();
+  }
+
+  // Sum the id numbers
+  function sumIds(idNums) {
+    return idNums.reduce(function(a, b) {
+      return a + b;
+    });
+  }
+
+  function checkWinningTurn() {
+    // Sum all id numbers for objects of 'o' class
+    var oIds = getIds('.o');
+    var oIdNums = getIdNums(oIds);
+    var oSum = sumIds(oIdNums);
+
+    // Sum all id numbers for objects of 'x' class
+    var xIds = getIds('.x');
+    var xIdNums = getIdNums(xIds);
+    var xSum = sumIds(xIdNums);
+
+    // Compare oSum and xSum to the winning sum
+    if ( oSum === 65 ) {
+      alert("O is the winner!");
+    } else if ( xSum === 65 ) {
+      alert("X is the winner!");
+    }
+  }
 
 })
 
