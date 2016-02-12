9df39167ddeb2383128184c79f777aa4dd8d235e
karenjho <hokarenj@gmail.com>
2016-02-12 14:11:58 -0500

    Place X or O when user clicks on a cell

--- nc.js
+++ nc.js
@@ -1,6 +1,33 @@
 $(document).on('ready', function() {
 
+  // Count the turns taken
+  var turn = 0;
 
+  // Place X or O in a clicked cell, depending on the turn
+  $('td').one('click', function() {
+
+    // We will need to perform multiples functions on the clicked td
+    // So we will store the jQuery function $(this) in a variable
+    var self = $(this)
+
+    // How to alternate between X and O?
+    // Assign X or O depending on whether the turn is even or odd
+      // i.e. 0 % 2 = 0;
+      //      1 % 2 = 1;
+      //      2 % 2 = 0;
+      //      3 % 2 = 1;
+      //      4 % 2 = 0;
+    // 0 is false-y
+    // 1 is truth-y
+
+    if (turn % 2) {
+      self.html('O').addClass('o');
+    } else {
+      self.html('X').addClass('x');
+    }
+
+    turn++ // Increment the turn counter
+  });
 
 })
 
