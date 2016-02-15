1273a732e02d61de655803c2ce49072754d69b03
karenjho <hokarenj@gmail.com>
2016-02-15 13:00:50 -0500

    Add comments to js for clarity

--- nc.js
+++ nc.js
@@ -2,11 +2,13 @@ $(document).on('ready', function() {
 
   // Count the turns taken
   var turn = 0;
-
   // Declare the size of the game board
   var gameBoard = 5;
+  // Calculate the minimum number of turns needed to win
+  var minWinningTurn = ( gameboard * 2 ) - 1
 
-  // Place X or O in a clicked cell, depending on the turn
+  // When someone clicks a cell, define the function when the click event occurs
+  // .one() only allows the event function to be performed once
   $('td').one('click', function() {
 
     // We will need to perform multiples functions on the clicked td
@@ -23,6 +25,7 @@ $(document).on('ready', function() {
     // 0 is false-y
     // 1 is truth-y
 
+    // Place X or O in a clicked cell, depending on the turn
     if (turn % 2) {
       self.html('O').addClass('o');
     } else {
@@ -30,7 +33,7 @@ $(document).on('ready', function() {
     }
 
     // Figure out whether the turn was a winning one
-    if (turn > 3 ) {
+    if (turn > minWinningTurn ) {
       checkWinningTurn();
     }
 
@@ -128,9 +131,13 @@ $(document).on('ready', function() {
 
   // Check if a player has a winning line
   function checkForWinLine(rowCount, colCount, descDiagCount, ascDiagCount) {
+              // First check if any of the player rows is as wide as the game board
     return  ( rowCount.includes(gameBoard) ||
+              // Then check if any of the player columns is as tall as the game board
               colCount.includes(gameBoard) ||
+              // Next check if the player has enough descending diagonal cells (equal to the game board)
               ( descDiagCount.length === gameBoard ) ||
+              // Finally check if the player has enough ascending diagonal cells (equal to the game board)
               ( ascDiagCount.length === gameBoard )
             );
   }
@@ -141,10 +148,13 @@ $(document).on('ready', function() {
     var oPlayerIds = getIds('.o');
     // Strip out coordinates from Player O's id values
     var oPlayerCoords = getCoords(oPlayerIds);
-
+    // Create an object that counts how often Player O appears in each row of the board ( key = row, value = appearance )
     var oPlayerRows = countRow(oPlayerCoords);
+    // Create an object that counts how often Player O appears in each column of the board ( key = column, value = appearance )
     var oPlayerCols = countCol(oPlayerCoords);
+    // Create an array of all the 'true' occurences when Player O's coordinate matches the descending diagonal
     var oPlayerDescDiagonal = countDescDiagonal(oPlayerCoords);
+    // Create an array of all the 'true' occurences when Player O's coordinate matches the ascending diagonal
     var oPlayerAscDiagonal = countAscDiagonal(oPlayerCoords);
 
     // Check for Player O wins
@@ -154,19 +164,22 @@ $(document).on('ready', function() {
     var xPlayerIds = getIds('.x');
     // Strip out coordinates from Player X's id values
     var xPlayerCoords = getCoords(xPlayerIds);
-
+    // Create an object that counts how often Player X appears in each row of the board ( key = row, value = appearance )
     var xPlayerRows = countRow(xPlayerCoords);
+    // Create an object that counts how often Player X appears in each column of the board ( key = column, value = appearance )
     var xPlayerCols = countCol(xPlayerCoords);
+    // Create an array of all the 'true' occurences when Player X's coordinate matches the descending diagonal
     var xPlayerDescDiagonal = countDescDiagonal(xPlayerCoords);
+    // Create an array of all the 'true' occurences when Player X's coordinate matches the ascending diagonal
     var xPlayerAscDiagonal = countAscDiagonal(xPlayerCoords);
 
     // Check for Player X wins
     var xPlayerWin = checkForWinLine(xPlayerRows, xPlayerCols, xPlayerDescDiagonal, xPlayerAscDiagonal);
 
     // If Player O wins, alert that Player O is winner.
-    // If Player X wins, alert that Player O is winner.
     if ( oPlayerWin ) {
       alert( "O is the winner!" );
+    // If Player X wins, alert that Player O is winner.
     } else if ( xPlayerWin ) {
       alert( "X is the winner!" );
     }
